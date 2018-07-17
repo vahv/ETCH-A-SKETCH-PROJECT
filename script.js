5c6b0fbf3350dd7d1f3c915e1b9834d2ceef3a58
Victor Herrera <victor.herrera.sc@itszapopan.edu.mx>
2018-07-17 15:38:09 -0500

    hovering over a colored square instead of changing to another random color, now it substracts ten percent of its color until it's left completely black

--- script.js
+++ script.js
@@ -13,7 +13,23 @@ function createGridElements(gridDimension, container){
 
 
 function changeBackground(){
-    this.style.cssText = 'background-color: #0a5688d5';
+    if(this.style.backgroundColor == ""){
+        const bgColor = `rgb(${randRGB()},${randRGB()},${randRGB()})`
+        
+        this.style.backgroundColor = bgColor;
+        this.setAttribute("data-bgcolor", bgColor);
+    }
+    else{
+        const originalBgColor = this.dataset.bgcolor;
+        const currentBgColor = this.style.backgroundColor;
+        const orginalRGBvals = getRGBValues(originalBgColor);
+        const currentRGBvals = getRGBValues(currentBgColor);
+        const tenPercent = 10;
+        const updatedRGBvals = substractPercentValues(currentRGBvals, getPercentages(orginalRGBvals, tenPercent));
+        
+        this.style.backgroundColor = `rgb(${updatedRGBvals[0]}, ${updatedRGBvals[1]}, ${updatedRGBvals[2]})`;
+
+    }
 }
 
 function changeGridlayout(){
@@ -32,4 +48,22 @@ function deleteGridElements(gridContainer){
 
 function setGridLayout(gridSize){
     document.documentElement.style.setProperty('--grid-size',gridSize);
+}
+
+function randRGB(){
+    return Math.floor((Math.random() * 255) + 1) ;
+}
+
+function getRGBValues(rgbString){
+    const regex = /\b([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\b/g;
+    return rgbString.match(regex);
+}
+
+function getPercentages(numbersArray, percentage){
+    percentage /= 100;
+    return numbersArray.map((number) => {return Math.ceil(number*percentage)});
+}
+
+function substractPercentValues(arrNumbs, percentValues){
+    return arrNumbs.map((number,index) => (number-percentValues[index] >=0 ?number-percentValues[index]:0));
 }
